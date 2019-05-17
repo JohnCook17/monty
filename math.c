0a8a64c729d5beda451981624e05d7331c179ef3
JohnCook17 <655@holbertonschool.com>
2019-05-17 05:48:02 +0000

    4

--- math.c
+++ math.c
@@ -4,11 +4,18 @@
  * @stack1: stack to add values of
  * @line_number: not really used
  */
-void _add(stack_t **stack1, int line_number)
+void _add(stack_t **stack, int line_number)
 {
-	(void)(stack1);
-	(void)(line_number);
-	printf("add\n");
+	stack_t *tmp = *stack;
+
+	if (*stack == NULL || !(*stack)->next)
+        {
+                fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+                exit(EXIT_FAILURE);
+        }
+	*stack = (*stack)->next;
+	tmp->next->n += tmp->n;
+	free (tmp);
 }
 /**
  * _nop - no op
