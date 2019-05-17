0990c91c749270805e62690cc694df050a76663b
JohnCook17 <655@holbertonschool.com>
2019-05-17 05:16:47 +0000

    fixed a segfault

--- push_pop.c
+++ push_pop.c
@@ -62,13 +62,11 @@ void _pop(stack_t **stack, int line_number)
 	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pop an empty stack", line_number);
-                _free(&global.head);
-                fclose(global.file);
                 exit(EXIT_FAILURE);
 	}
 	*stack = (*stack)->next;
 	free(tmp);
-	if (stack != NULL)
+	if (*stack != NULL)
 		(*stack)->prev = NULL;
 }
 /**
